11f574f01bfdd340f1d72f9562a85ad3155f072b
Nara Neang <phearom.neang@gmail.com>
2017-08-29 17:38:57 -0700

    update method

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,31 +12,36 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            GetStoreProcedure();
-            return;
 
             var con = new OracleConnection("user id=phearom; password=123;data source=localhost:1521/db002");
-            var cmd = new OracleCommand("select * from tblstudent", con);
             con.Open();
-            var dr = cmd.ExecuteReader();
-            Console.WriteLine("Get data from Oracle");
-            while (dr.Read())
+            try
             {
-                Console.WriteLine(dr.GetString(1));
+                var cmd = new OracleCommand("select * from tblstudent", con);
+                var dr = cmd.ExecuteReader();
+                Console.WriteLine("Get data from Oracle");
+                while (dr.Read())
+                {
+                    Console.WriteLine(dr.GetString(1));
+                }
+                con.Close();
             }
-            con.Close();
+            catch (Exception ex)
+            {
+                con.Close();
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadKey();
         }
         static void GetStoreProcedure()
         {
+            var con = new OracleConnection("user id=phearom; password=123;data source=localhost:1521/db002");
+            con.Open();
             try
             {
-
-                var con = new OracleConnection("user id=phearom; password=123;data source=localhost:1521/db002");
                 var cmd = new OracleCommand("SP_GETALLSTUDENT", con);
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
                 cmd.Parameters.Add(new OracleParameter("cursorParam", OracleDbType.RefCursor, ParameterDirection.Output));
-                con.Open();
                 var dr = cmd.ExecuteReader();
                 Console.WriteLine("Get data from Oracle using Store procedure ref cursor");
                 while (dr.Read())
@@ -45,12 +50,13 @@ namespace ConsoleApp
                 }
                 con.Close();
                 Console.ReadKey();
-
             }
             catch (Exception ex)
             {
+                con.Close();
                 Console.WriteLine(ex.Message);
             }
+            Console.ReadKey();
         }
     }
 }
